8059aff947861441e9d2411afd72ebecb29e57f3
katono <katono123@gmail.com>
2019-04-07 22:38:03 +0900

    Fix *OSWrapper::init()

--- platform/ItronOSWrapper/ItronOSWrapper.cpp
+++ platform/ItronOSWrapper/ItronOSWrapper.cpp
@@ -4,11 +4,13 @@
 #include "ItronEventFlagFactory.h"
 #include "ItronFixedMemoryPoolFactory.h"
 #include "ItronVariableMemoryPoolFactory.h"
+#include "ItronPeriodicTimerFactory.h"
 #include "OSWrapper/Thread.h"
 #include "OSWrapper/Mutex.h"
 #include "OSWrapper/EventFlag.h"
 #include "OSWrapper/FixedMemoryPool.h"
 #include "OSWrapper/VariableMemoryPool.h"
+#include "OSWrapper/PeriodicTimer.h"
 
 namespace ItronOSWrapper {
 
@@ -19,12 +21,14 @@ void init()
 	static ItronEventFlagFactory theEventFlagFactory;
 	static ItronFixedMemoryPoolFactory theFixedMemoryPoolFactory;
 	static ItronVariableMemoryPoolFactory theVariableMemoryPoolFactory;
+	static ItronPeriodicTimerFactory thePeriodicTimerFactory;
 
 	OSWrapper::registerThreadFactory(&theThreadFactory);
 	OSWrapper::registerMutexFactory(&theMutexFactory);
 	OSWrapper::registerEventFlagFactory(&theEventFlagFactory);
 	OSWrapper::registerFixedMemoryPoolFactory(&theFixedMemoryPoolFactory);
 	OSWrapper::registerVariableMemoryPoolFactory(&theVariableMemoryPoolFactory);
+	OSWrapper::registerPeriodicTimerFactory(&thePeriodicTimerFactory);
 }
 
 }
--- platform/WindowsOSWrapper/WindowsOSWrapper.cpp
+++ platform/WindowsOSWrapper/WindowsOSWrapper.cpp
@@ -4,11 +4,13 @@
 #include "WindowsEventFlagFactory.h"
 #include "WindowsFixedMemoryPoolFactory.h"
 #include "WindowsVariableMemoryPoolFactory.h"
+#include "WindowsPeriodicTimerFactory.h"
 #include "OSWrapper/Thread.h"
 #include "OSWrapper/Mutex.h"
 #include "OSWrapper/EventFlag.h"
 #include "OSWrapper/FixedMemoryPool.h"
 #include "OSWrapper/VariableMemoryPool.h"
+#include "OSWrapper/PeriodicTimer.h"
 
 namespace WindowsOSWrapper {
 
@@ -19,12 +21,14 @@ void init(int lowestPriority, int highestPriority)
 	static WindowsEventFlagFactory theEventFlagFactory;
 	static WindowsFixedMemoryPoolFactory theFixedMemoryPoolFactory;
 	static WindowsVariableMemoryPoolFactory theVariableMemoryPoolFactory;
+	static WindowsPeriodicTimerFactory thePeriodicTimerFactory;
 
 	OSWrapper::registerThreadFactory(&theThreadFactory);
 	OSWrapper::registerMutexFactory(&theMutexFactory);
 	OSWrapper::registerEventFlagFactory(&theEventFlagFactory);
 	OSWrapper::registerFixedMemoryPoolFactory(&theFixedMemoryPoolFactory);
 	OSWrapper::registerVariableMemoryPoolFactory(&theVariableMemoryPoolFactory);
+	OSWrapper::registerPeriodicTimerFactory(&thePeriodicTimerFactory);
 }
 
 }
