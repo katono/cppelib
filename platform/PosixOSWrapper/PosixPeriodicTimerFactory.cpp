d9236254a64ed3c0a4a0d8d8c52ba97c02b4c9c7
katono <katono123@gmail.com>
2019-04-12 23:16:53 +0900

    Fix: timer_settime() is reset in the timer callback.

--- platform/PosixOSWrapper/PosixPeriodicTimerFactory.cpp
+++ platform/PosixOSWrapper/PosixPeriodicTimerFactory.cpp
@@ -6,7 +6,6 @@
 #include <memory>
 #include <time.h>
 #include <signal.h>
-#include <pthread.h>
 
 namespace PosixOSWrapper {
 
@@ -19,39 +18,61 @@ private:
 	bool m_started;
 	timer_t m_timer;
 	struct sigevent m_sigEvent;
-	std::unique_ptr<OSWrapper::EventFlag, decltype(&OSWrapper::EventFlag::destroy)> m_evFinished;
+	std::unique_ptr<OSWrapper::EventFlag, decltype(&OSWrapper::EventFlag::destroy)> m_flags;
+
+	static const OSWrapper::EventFlag::Pattern EV_FINISHED;
+	static const OSWrapper::EventFlag::Pattern EV_STOPPED;
 
 	static void timerCallback(union sigval sv)
 	{
 		PosixPeriodicTimer* t = static_cast<PosixPeriodicTimer*>(sv.sival_ptr);
 		if (t != nullptr) {
-			t->m_evFinished->resetAll();
-			t->timerMain();
-			t->m_evFinished->setAll();
+			t->timerFunc();
 		}
+	}
+
+	void timerFunc()
+	{
+		m_flags->reset(EV_FINISHED);
+
+		timerMain();
+
+		if ((m_flags->getCurrentPattern() & EV_STOPPED) != 0) {
+			const time_t sec = m_periodInMillis / 1000;
+			const long nsec = (m_periodInMillis % 1000) * 1000000;
+			struct itimerspec its;
+			its.it_value.tv_sec = sec;
+			its.it_value.tv_nsec = nsec;
+			its.it_interval.tv_sec = 0;
+			its.it_interval.tv_nsec = 0;
+			timer_settime(m_timer, 0, &its, nullptr);
+		}
+
+		m_flags->set(EV_FINISHED);
 	}
 
 public:
 	PosixPeriodicTimer(OSWrapper::Runnable* r, unsigned long periodInMillis, const char* name)
 	: PeriodicTimer(r), m_periodInMillis(periodInMillis), m_name(name)
 	, m_mutex(), m_started(false), m_timer(), m_sigEvent()
-	, m_evFinished(OSWrapper::EventFlag::create(false), &OSWrapper::EventFlag::destroy)
+	, m_flags(OSWrapper::EventFlag::create(false), &OSWrapper::EventFlag::destroy)
 	{
-		m_evFinished->setAll();
+		m_flags->set(EV_FINISHED);
 
 		m_sigEvent.sigev_value.sival_ptr = this;
 		m_sigEvent.sigev_notify = SIGEV_THREAD;
 		m_sigEvent.sigev_notify_function = timerCallback;
-		m_sigEvent.sigev_notify_attributes = NULL;
+		m_sigEvent.sigev_notify_attributes = nullptr;
 		timer_create(CLOCK_REALTIME, &m_sigEvent, &m_timer);
 	}
 
 	~PosixPeriodicTimer()
 	{
-		m_evFinished->waitAny();
+		stop();
+		m_flags->wait(EV_FINISHED, OSWrapper::EventFlag::OR, nullptr);
 		timer_delete(m_timer);
 		OSWrapper::Thread::sleep(m_periodInMillis);
-		m_evFinished->waitAny();
+		m_flags->wait(EV_FINISHED, OSWrapper::EventFlag::OR, nullptr);
 	}
 
 	void start()
@@ -60,14 +81,15 @@ public:
 		if (m_started) {
 			return;
 		}
+		m_flags->reset(EV_STOPPED);
 		const time_t sec = m_periodInMillis / 1000;
 		const long nsec = (m_periodInMillis % 1000) * 1000000;
 		struct itimerspec its;
 		its.it_value.tv_sec = sec;
 		its.it_value.tv_nsec = nsec;
-		its.it_interval.tv_sec = sec;
-		its.it_interval.tv_nsec = nsec;
-		timer_settime(m_timer, 0, &its, NULL);
+		its.it_interval.tv_sec = 0;
+		its.it_interval.tv_nsec = 0;
+		timer_settime(m_timer, 0, &its, nullptr);
 		m_started = true;
 	}
 
@@ -77,12 +99,7 @@ public:
 		if (!m_started) {
 			return;
 		}
-		struct itimerspec its;
-		its.it_value.tv_sec = 0;
-		its.it_value.tv_nsec = 0;
-		its.it_interval.tv_sec = 0;
-		its.it_interval.tv_nsec = 0;
-		timer_settime(m_timer, 0, &its, NULL);
+		m_flags->set(EV_STOPPED);
 		m_started = false;
 	}
 
@@ -110,6 +127,9 @@ public:
 	}
 
 };
+
+const OSWrapper::EventFlag::Pattern PosixPeriodicTimer::EV_FINISHED = 0x0001U;
+const OSWrapper::EventFlag::Pattern PosixPeriodicTimer::EV_STOPPED = 0x0002U;
 
 
 PosixPeriodicTimerFactory::PosixPeriodicTimerFactory()
