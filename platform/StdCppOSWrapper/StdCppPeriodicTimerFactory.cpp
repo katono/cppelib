8867cfd3f0a2707eb06bbbc5da7a34ce33776ab0
katono <katono123@gmail.com>
2019-04-14 23:21:48 +0900

    Fix deadlock bug

--- platform/StdCppOSWrapper/StdCppPeriodicTimerFactory.cpp
+++ platform/StdCppOSWrapper/StdCppPeriodicTimerFactory.cpp
@@ -28,9 +28,11 @@ private:
 	std::unique_ptr<OSWrapper::Thread, decltype(&OSWrapper::Thread::destroy)> m_thread;
 	mutable std::mutex m_mutex;
 
+	std::condition_variable m_condActive;
 	std::condition_variable m_condStarted;
 	std::condition_variable m_condStopped;
 	bool m_isActive;
+	bool m_started;
 	bool m_stopped;
 	bool m_endThreadRequested;
 
@@ -39,7 +41,9 @@ private:
 		while (true) {
 			{
 				std::unique_lock<std::mutex> lock(m_mutex);
-				m_condStarted.wait(lock, [this] { return m_isActive || m_endThreadRequested; });
+				m_condActive.wait(lock, [this] { return m_isActive || m_endThreadRequested; });
+				m_started = true;
+				m_condStarted.notify_all();
 				if (m_endThreadRequested) {
 					break;
 				}
@@ -47,7 +51,7 @@ private:
 			while (true) {
 				{
 					std::unique_lock<std::mutex> lock(m_mutex);
-					if (m_condStarted.wait_for(lock, std::chrono::milliseconds(m_periodInMillis),
+					if (m_condActive.wait_for(lock, std::chrono::milliseconds(m_periodInMillis),
 								[this] { return !m_isActive || m_endThreadRequested; })) {
 						m_stopped = true;
 						m_condStopped.notify_all();
@@ -67,8 +71,8 @@ public:
 	: PeriodicTimer(r), m_periodInMillis(periodInMillis), m_name(name), 
 	  m_task(this), 
 	  m_thread(OSWrapper::Thread::create(&m_task), &OSWrapper::Thread::destroy), 
-	  m_mutex(), m_condStarted(), m_condStopped(), 
-	  m_isActive(false), m_stopped(true), m_endThreadRequested(false)
+	  m_mutex(), m_condActive(), m_condStarted(), m_condStopped(), 
+	  m_isActive(false), m_started(false), m_stopped(true), m_endThreadRequested(false)
 	{
 		m_thread->setPriority(OSWrapper::Thread::getHighestPriority());
 		m_thread->start();
@@ -83,7 +87,7 @@ public:
 		{
 			std::lock_guard<std::mutex> lock(m_mutex);
 			m_endThreadRequested = true;
-			m_condStarted.notify_all();
+			m_condActive.notify_all();
 		}
 		m_thread->wait();
 	}
@@ -97,17 +101,19 @@ public:
 		m_condStopped.wait(lock, [this] { return m_stopped; });
 		m_stopped = false;
 		m_isActive = true;
-		m_condStarted.notify_all();
+		m_condActive.notify_all();
 	}
 
 	void stop()
 	{
-		std::lock_guard<std::mutex> lock(m_mutex);
+		std::unique_lock<std::mutex> lock(m_mutex);
 		if (!m_isActive) {
 			return;
 		}
+		m_condStarted.wait(lock, [this] { return m_started; });
+		m_started = false;
 		m_isActive = false;
-		m_condStarted.notify_all();
+		m_condActive.notify_all();
 	}
 
 	bool isStarted() const
