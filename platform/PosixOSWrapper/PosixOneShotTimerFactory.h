75c3924a046ea0ea806619a9c16f4a347ff27550
katono <katono123@gmail.com>
2019-04-16 08:16:23 +0900

    Add PosixOSWrapper/PosixOneShotTimerFactory.h

---
+++ platform/PosixOSWrapper/PosixOneShotTimerFactory.h
@@ -0,0 +1,13 @@
+#ifndef POSIX_OS_WRAPPER_POSIX_ONESHOT_TIMER_FACTORY_H_INCLUDED
+#define POSIX_OS_WRAPPER_POSIX_ONESHOT_TIMER_FACTORY_H_INCLUDED
+
+#include "StdCppOSWrapper/StdCppOneShotTimerFactory.h"
+
+namespace PosixOSWrapper {
+
+class PosixOneShotTimerFactory : public StdCppOSWrapper::StdCppOneShotTimerFactory {
+};
+
+}
+
+#endif // POSIX_OS_WRAPPER_POSIX_ONESHOT_TIMER_FACTORY_H_INCLUDED
