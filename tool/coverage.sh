2011b0197e4b5e4da1f0248611504560b94ec892
katono <katono123@gmail.com>
2019-05-19 23:40:29 +0900

    Add clean option

--- tool/coverage.sh
+++ tool/coverage.sh
@@ -1,24 +1,35 @@
 #!/bin/sh
 
 if [ "$1" = "" ]; then
-	echo "Usage: $0 REPO_ROOT"
+	echo "Usage: $0 REPO_ROOT [clean]"
 	exit
 fi
 
 REPO_ROOT=$1
 cd ${REPO_ROOT}
 
+rm -rf coverage_report
+rm -f coverage.info
+
 BUILD_DIR=coverage_build
 BUILD_DIRS="mechanism/test/${BUILD_DIR} platform/test/cmake_StdCppOSWrapperTest/${BUILD_DIR} platform/test/cmake_PosixOSWrapperTest/${BUILD_DIR}"
 for dir in ${BUILD_DIRS}; do
-	rm -rf ${dir}
-	mkdir -p ${dir}
-	cd ${dir}
-	cmake .. -DCMAKE_CXX_FLAGS=--coverage -DCMAKE_EXE_LINKER_FLAGS=--coverage -DCMAKE_BUILD_TYPE=Debug
-	make
-	ctest -V
-	cd -
+	if [ "$2" = "clean" ]; then
+		rm -rf ${dir}
+	else
+		rm -rf ${dir}
+		mkdir -p ${dir}
+		cd ${dir}
+		cmake .. -DCMAKE_CXX_FLAGS=--coverage -DCMAKE_EXE_LINKER_FLAGS=--coverage -DCMAKE_BUILD_TYPE=Debug
+		make
+		ctest -V
+		cd -
+	fi
 done
+
+if [ "$2" = "clean" ]; then
+	exit
+fi
 
 lcov -d . -c -o coverage.info
 lcov -r coverage.info "*/CppUTest/*" "/usr/*" "*/test/*" -o coverage.info
