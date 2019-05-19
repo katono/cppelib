6e9c4a6b73a6c6d4de5ef364650eb3b99ff9af6e
katono <katono123@gmail.com>
2019-05-19 23:42:58 +0900

    Add run_test.sh

---
+++ tool/run_test.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ "$1" = "" ]; then
+	echo "Usage: $0 REPO_ROOT [clean|rebuild]"
+	exit
+fi
+
+REPO_ROOT=$1
+cd ${REPO_ROOT}
+
+BUILD_DIR=build
+BUILD_DIRS="mechanism/test/${BUILD_DIR} platform/test/cmake_StdCppOSWrapperTest/${BUILD_DIR} platform/test/cmake_PosixOSWrapperTest/${BUILD_DIR}"
+for dir in ${BUILD_DIRS}; do
+	if [ "$2" = "clean" ]; then
+		rm -rf ${dir}
+	else
+		if [ "$2" = "rebuild" ]; then
+			rm -rf ${dir}
+		fi
+		mkdir -p ${dir}
+		cd ${dir}
+		cmake ..
+		make
+		ctest -V
+		cd -
+	fi
+done
+
