7b6cb62ab6a9bd42ed7ae7e60a061e66b2ef7863
katono <katono123@gmail.com>
2019-05-19 23:16:30 +0900

    Fix to use coverage_build directory for cmake build

--- tool/coverage.sh
+++ tool/coverage.sh
@@ -8,24 +8,17 @@ fi
 REPO_ROOT=$1
 cd ${REPO_ROOT}
 
-cd mechanism/test
-cmake . -DCMAKE_CXX_FLAGS=--coverage -DCMAKE_EXE_LINKER_FLAGS=--coverage -DCMAKE_BUILD_TYPE=Debug
-make
-ctest -V
-cd -
-
-cd platform/test/cmake_StdCppOSWrapperTest
-cmake . -DCMAKE_CXX_FLAGS=--coverage -DCMAKE_EXE_LINKER_FLAGS=--coverage -DCMAKE_BUILD_TYPE=Debug
-make
-ctest -V
-cd -
-
-cd platform/test/cmake_PosixOSWrapperTest
-cmake . -DCMAKE_CXX_FLAGS=--coverage -DCMAKE_EXE_LINKER_FLAGS=--coverage -DCMAKE_BUILD_TYPE=Debug
-make
-ctest -V
-cd -
-
+BUILD_DIR=coverage_build
+BUILD_DIRS="mechanism/test/${BUILD_DIR} platform/test/cmake_StdCppOSWrapperTest/${BUILD_DIR} platform/test/cmake_PosixOSWrapperTest/${BUILD_DIR}"
+for dir in ${BUILD_DIRS}; do
+	rm -rf ${dir}
+	mkdir -p ${dir}
+	cd ${dir}
+	cmake .. -DCMAKE_CXX_FLAGS=--coverage -DCMAKE_EXE_LINKER_FLAGS=--coverage -DCMAKE_BUILD_TYPE=Debug
+	make
+	ctest -V
+	cd -
+done
 
 lcov -d . -c -o coverage.info
 lcov -r coverage.info "*/CppUTest/*" "/usr/*" "*/test/*" -o coverage.info
