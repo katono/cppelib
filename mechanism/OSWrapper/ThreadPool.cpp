f94f9f7778ce899b0b5e1ae200c64fa12e5f20a0
katono <katono123@gmail.com>
2019-12-11 22:22:14 +0900

    Add ThreadPool

---
+++ mechanism/OSWrapper/ThreadPool.cpp
@@ -0,0 +1,334 @@
+#include "ThreadPool.h"
+#include "MessageQueue.h"
+#include "EventFlag.h"
+#include "FixedMemoryPool.h"
+
+namespace OSWrapper {
+
+ThreadPool::ThreadPool(FixedMemoryPool* objPool, int defaultPriority)
+: m_freeRunnerQueue(0)
+, m_threads()
+, m_threadMemory(0)
+, m_threadMemoryPool(0)
+, m_runnerPool(0)
+, m_objPool(objPool)
+, m_uncaughtExceptionHandler(0)
+, m_defaultPriority(defaultPriority)
+{
+}
+
+ThreadPool::~ThreadPool()
+{
+}
+
+ThreadPool* ThreadPool::create(std::size_t maxThreads, std::size_t stackSize/*= 0U*/, int defaultPriority/*= Thread::getNormalPriority()*/)
+{
+	const std::size_t objSize = sizeof(ThreadPool);
+	FixedMemoryPool* objPool = FixedMemoryPool::create(objSize,
+			FixedMemoryPool::getRequiredMemorySize(objSize, 1U));
+	if (objPool == 0) {
+		return 0;
+	}
+
+	void* p = objPool->allocate();
+	if (p == 0) {
+		FixedMemoryPool::destroy(objPool);
+		return 0;
+	}
+	ThreadPool* tp = new(p) ThreadPool(objPool, defaultPriority);
+
+	if (!tp->constructMembers(maxThreads, stackSize)) {
+		tp->~ThreadPool();
+		objPool->deallocate(tp);
+		FixedMemoryPool::destroy(objPool);
+		return 0;
+	}
+	return tp;
+}
+
+bool ThreadPool::constructMembers(std::size_t maxThreads, std::size_t stackSize)
+{
+	const std::size_t threadMemoryPoolSize = sizeof(Thread*) * maxThreads;
+
+	m_freeRunnerQueue = TaskRunnerMQ::create(maxThreads);
+	if (m_freeRunnerQueue == 0) {
+		goto failed;
+	}
+
+	m_threadMemoryPool = FixedMemoryPool::create(threadMemoryPoolSize,
+			FixedMemoryPool::getRequiredMemorySize(threadMemoryPoolSize, 1U));
+	if (m_threadMemoryPool == 0) {
+		goto failed;
+	}
+	m_threadMemory = m_threadMemoryPool->allocate();
+	if (m_threadMemory == 0) {
+		goto failed;
+	}
+	m_threads.init(m_threadMemory, threadMemoryPoolSize);
+
+	m_runnerPool = FixedMemoryPool::create(sizeof(TaskRunner),
+			FixedMemoryPool::getRequiredMemorySize(sizeof(TaskRunner), maxThreads));
+	if (m_runnerPool == 0) {
+		goto failed;
+	}
+
+	for (std::size_t i = 0U; i < maxThreads; ++i) {
+		void* p = m_runnerPool->allocate();
+		if (p == 0) {
+			goto failed;
+		}
+
+		EventFlag* e = EventFlag::create(false);
+		if (e == 0) {
+			m_runnerPool->deallocate(p);
+			goto failed;
+		}
+
+		TaskRunner* runner = new(p) TaskRunner(e, this);
+
+		Thread* t = Thread::create(runner, m_defaultPriority, stackSize);
+		if (t == 0) {
+			EventFlag::destroy(e);
+			runner->~TaskRunner();
+			m_runnerPool->deallocate(p);
+			goto failed;
+		}
+		runner->setThread(t);
+
+		m_threads.push_back(t);
+		m_freeRunnerQueue->send(runner);
+	}
+	return true;
+
+failed:
+	destroyMembers();
+	return false;
+}
+
+void ThreadPool::destroy(ThreadPool* p)
+{
+	if (p == 0) {
+		return;
+	}
+	FixedMemoryPool* objPool = p->m_objPool;
+	p->waitThreads();
+	p->destroyMembers();
+	p->~ThreadPool();
+	objPool->deallocate(p);
+	FixedMemoryPool::destroy(objPool);
+}
+
+void ThreadPool::waitThreads()
+{
+	for (std::size_t i = 0U; i < m_threads.size(); ++i) {
+		m_threads[i]->wait();
+	}
+}
+
+void ThreadPool::destroyMembers()
+{
+	destroyRunners();
+	destroyThreads();
+
+	FixedMemoryPool::destroy(m_runnerPool);
+	FixedMemoryPool::destroy(m_threadMemoryPool);
+	TaskRunnerMQ::destroy(m_freeRunnerQueue);
+}
+
+void ThreadPool::destroyRunners()
+{
+	if (m_freeRunnerQueue == 0) {
+		return;
+	}
+	if (m_runnerPool == 0) {
+		return;
+	}
+	while (true) {
+		TaskRunner* runner = 0;
+		Error err = m_freeRunnerQueue->tryReceive(&runner);
+		if (err != OK) {
+			// empty
+			break;
+		}
+		EventFlag::destroy(runner->getEventFlag());
+		Thread::destroy(runner->getThread());
+		runner->~TaskRunner();
+		m_runnerPool->deallocate(runner);
+	}
+}
+
+void ThreadPool::destroyThreads()
+{
+	if (m_threadMemoryPool == 0) {
+		return;
+	}
+	if (m_threadMemory == 0) {
+		return;
+	}
+	m_threadMemoryPool->deallocate(m_threadMemory);
+}
+
+Error ThreadPool::start(Runnable* task, WaitGuard* waiter/*= 0*/, int priority/*= Thread::INHERIT_PRIORITY*/)
+{
+	return timedStart(task, Timeout::FOREVER, waiter, priority);
+}
+
+Error ThreadPool::tryStart(Runnable* task, WaitGuard* waiter/*= 0*/, int priority/*= Thread::INHERIT_PRIORITY*/)
+{
+	return timedStart(task, Timeout::POLLING, waiter, priority);
+}
+
+Error ThreadPool::timedStart(Runnable* task, Timeout tmout, WaitGuard* waiter/*= 0*/, int priority/*= Thread::INHERIT_PRIORITY*/)
+{
+	if (task == 0) {
+		return InvalidParameter;
+	}
+	TaskRunner* runner = 0;
+	const Error recvErr = m_freeRunnerQueue->timedReceive(&runner, tmout);
+	if (recvErr != OK) {
+		return recvErr;
+	}
+
+	Thread* t = runner->getThread();
+	const Error waitErr = t->timedWait(tmout);
+	if (waitErr != OK) {
+		return waitErr;
+	}
+
+	runner->startThread(task, priority, (waiter != 0));
+
+	if (waiter != 0) {
+		waiter->m_runner = runner;
+	}
+	return OK;
+}
+
+void ThreadPool::releaseRunner(TaskRunner* runner)
+{
+	m_freeRunnerQueue->send(runner);
+}
+
+void ThreadPool::setUncaughtExceptionHandler(Thread::UncaughtExceptionHandler* handler)
+{
+	m_uncaughtExceptionHandler = handler;
+	for (std::size_t i = 0U; i < m_threads.size(); ++i) {
+		m_threads[i]->setUncaughtExceptionHandler(handler);
+	}
+}
+
+Thread::UncaughtExceptionHandler* ThreadPool::getUncaughtExceptionHandler() const
+{
+	return m_uncaughtExceptionHandler;
+}
+
+
+ThreadPool::TaskRunner::TaskRunner(EventFlag* ev, ThreadPool* tp)
+: m_task(0)
+, m_thread(0)
+, m_ev(ev)
+, m_needsWaiting(false)
+, m_tp(tp)
+{
+}
+
+ThreadPool::TaskRunner::~TaskRunner()
+{
+}
+
+void ThreadPool::TaskRunner::startThread(Runnable* task, int priority, bool needsWaiting)
+{
+	m_task = task;
+	m_needsWaiting = needsWaiting;
+	m_thread->setPriority(priority);
+	m_thread->start();
+}
+
+void ThreadPool::TaskRunner::run()
+{
+	try {
+		invoke();
+	} catch (...) {
+		afterInvoke();
+		throw;
+	}
+	afterInvoke();
+
+}
+
+void ThreadPool::TaskRunner::invoke()
+{
+	if (m_task != 0) {
+		m_task->run();
+	}
+}
+
+void ThreadPool::TaskRunner::afterInvoke()
+{
+	m_thread->setPriority(m_tp->getDefaultPriority());
+	if (m_needsWaiting) {
+		notifyFinished();
+	} else {
+		release();
+	}
+}
+
+void ThreadPool::TaskRunner::notifyFinished()
+{
+	m_ev->setAll();
+}
+
+void ThreadPool::TaskRunner::release()
+{
+	m_ev->resetAll();
+	m_tp->releaseRunner(this);
+}
+
+Error ThreadPool::TaskRunner::timedWait(Timeout tmout)
+{
+	return m_ev->timedWaitAny(tmout);
+}
+
+
+ThreadPool::WaitGuard::WaitGuard()
+: m_runner(0)
+{
+}
+
+ThreadPool::WaitGuard::~WaitGuard()
+{
+	release();
+}
+
+void ThreadPool::WaitGuard::release()
+{
+	if (m_runner != 0) {
+		wait();
+		m_runner->release();
+		m_runner = 0;
+	}
+}
+
+Error ThreadPool::WaitGuard::wait()
+{
+	return timedWait(Timeout::FOREVER);
+}
+
+Error ThreadPool::WaitGuard::tryWait()
+{
+	return timedWait(Timeout::POLLING);
+}
+
+Error ThreadPool::WaitGuard::timedWait(Timeout tmout)
+{
+	if (m_runner == 0) {
+		return OK;
+	}
+	return m_runner->timedWait(tmout);
+}
+
+bool ThreadPool::WaitGuard::isValid() const
+{
+	return m_runner != 0;
+}
+
+}
---
+++ mechanism/OSWrapper/ThreadPool.h
@@ -0,0 +1,253 @@
+#ifndef OS_WRAPPER_THREAD_POOL_H_INCLUDED
+#define OS_WRAPPER_THREAD_POOL_H_INCLUDED
+
+#include <cstddef>
+#include "Runnable.h"
+#include "Thread.h"
+#include "Container/PreallocatedVector.h"
+
+namespace OSWrapper {
+
+template <typename T> class MessageQueue;
+class EventFlag;
+class FixedMemoryPool;
+
+/*!
+ * @brief Class of thread pool pattern
+ *
+ * ThreadPool creates some threads to call tasks asynchronously.
+ * When asynchronous tasks are finished, used threads are reused later instead of destroyed.
+ * An asynchronous task needs to be a class that implements OSWrapper::Runnable interface.
+ * You can set a thread priority of the asynchronous task when you call start().
+ *
+ * If you choose to use a WaitGuard when you call start(), you need to wait for the asynchronous task finished.
+ * After you check the task finished, you can take data from the task object without mutex.
+ *
+ * If you choose not to use a WaitGuard when you call start(), you don't have to wait for the task finished.
+ * But the destruction of the task object must be later than the task finished.
+ * Otherwise the access violation may occur that the thread may access to the destroyed object.
+ */
+class ThreadPool {
+private:
+	class TaskRunner : public Runnable {
+	public:
+		void release();
+		Error timedWait(Timeout tmout);
+
+	private:
+		friend class ThreadPool;
+
+		TaskRunner(EventFlag* ev, ThreadPool* tp);
+		~TaskRunner();
+		void startThread(Runnable* task, int priority, bool needsWaiting);
+		void run();
+		void invoke();
+		void afterInvoke();
+		void notifyFinished();
+		void setThread(Thread* t) { m_thread = t; }
+		Thread* getThread() const { return m_thread; }
+		EventFlag* getEventFlag() const { return m_ev; }
+
+		Runnable* m_task;
+		Thread* m_thread;
+		EventFlag* m_ev;
+		bool m_needsWaiting;
+		ThreadPool* m_tp;
+
+		TaskRunner(const TaskRunner&);
+		TaskRunner& operator=(const TaskRunner&);
+	};
+
+public:
+	/*!
+	 * @brief Class to wait for the asynchronous task finished
+	 *
+	 * WaitGuard has a validity state, valid or invalid.
+	 * After constuction, the state is invalid.
+	 * The state becomes valid by ThreadPool::start(), ThreadPool::tryStart(), or ThreadPool::timedStart().
+	 * If the state is valid, the thread that owns this WaitGuard must wait() until the started asynchronous task finished and must release() the WaitGuard.
+	 * After release(), the state becomes invalid.
+	 * 
+	 * WaitGuard can't be copied.
+	 */
+	class WaitGuard {
+	public:
+		/*!
+		 * @brief Constructor of WaitGuard
+		 * @post isValid() == false
+		 */
+		WaitGuard();
+
+		/*!
+		 * @brief Destructor of WaitGuard
+		 *
+		 * Destructor calls release() if the validity state is valid.
+		 */
+		~WaitGuard();
+
+		/*!
+		 * @brief Call wait(), release the resource, and turn the valid state to invalid
+		 * @post isValid() == false
+		 * 
+		 * @note If the state is invalid, this method does nothing.
+		 */
+		void release();
+
+		/*!
+		 * @brief Block the current thread until the asynchronous task finished
+		 * @retval OK Success. The asynchronous task has finished
+		 * @retval CalledByNonThread Called from non thread context (interrupt handler, timer, etc)
+		 *
+		 * @note If the state is invalid, this method returns OK immediately.
+		 * @note Same as timedWait(Timeout::FOREVER)
+		 */
+		Error wait();
+
+		/*!
+		 * @brief Query without blocking whether the asynchronous task finished
+		 * @retval OK The asynchronous task has already finished
+		 * @retval TimedOut The asynchronous task has not finished yet
+		 *
+		 * @note If the state is invalid, this method returns OK immediately.
+		 * @note Same as timedWait(Timeout::POLLING)
+		 */
+		Error tryWait();
+
+		/*!
+		 * @brief Block the current thread until the asynchronous task finished but only within the limited time
+		 * @param tmout The limited time
+		 * @retval OK Success. The asynchronous task has finished
+		 * @retval TimedOut The limited time was elapsed
+		 * @retval CalledByNonThread Called from non thread context (interrupt handler, timer, etc)
+		 *
+		 * @note If the state is invalid, this method returns OK immediately.
+		 * @note If \p tmout is Timeout::POLLING then this method queries the state without blocking.
+		 * @note If \p tmout is Timeout::FOREVER then this method waits forever until the condition is satisfied.
+		 */
+		Error timedWait(Timeout tmout);
+
+		/*!
+		 * @brief Return true if the validity state is valid
+		 * @retval true The state is valid
+		 * @retval false The state is invalid
+		 */
+		bool isValid() const;
+
+	private:
+		friend class ThreadPool;
+		TaskRunner* m_runner;
+
+		WaitGuard(const WaitGuard&);
+		WaitGuard& operator=(const WaitGuard&);
+	};
+
+	/*!
+	 * @brief Create a ThreadPool object
+	 * @param maxThreads Number of max threads that can execute concurrently
+	 * @param stackSize Stack size of threads. If zero then sets default stack size of RTOS.
+	 * @param defaultPriority First, the thread priority is \p defaultPriority when the threads are created. Then the priority is changed by the parameter of start() when an asynchronous task is started. After the asynchronous task finishes, the priority returns to \p defaultPriority.
+	 * @return If this method succeeds then returns a pointer of ThreadPool object, else returns null pointer
+	 */
+	static ThreadPool* create(std::size_t maxThreads, std::size_t stackSize = 0U, int defaultPriority = Thread::getNormalPriority());
+
+	/*!
+	 * @brief Destroy a ThreadPool object
+	 *
+	 * This method waits for all of the asynchronous tasks finished and destroys all of the threads.
+	 *
+	 * @param p Pointer of ThreadPool object created by ThreadPool::create()
+	 *
+	 * @note If p is null pointer, do nothing.
+	 */
+	static void destroy(ThreadPool* p);
+
+	/*!
+	 * @brief Start an asynchronous task
+	 * @param task Asynchronous task
+	 * @param waiter Pointer of variable of WaitGuard object that state is invalid. If this method succeeds, the WaitGuard object becomes valid. If you don't want to wait for the task finished, specify null pointer or omit this.
+	 * @param priority Thread priority of the asynchronous task
+	 * @retval OK Success. A free thread has been allocated and started
+	 * @retval InvalidParameter \p task is null pointer
+	 * @retval CalledByNonThread Called from non thread context (interrupt handler, timer, etc)
+	 *
+	 * @note Same as timedStart(task, Timeout::FOREVER, waiter, priority)
+	 */
+	Error start(Runnable* task, WaitGuard* waiter = 0, int priority = Thread::INHERIT_PRIORITY);
+
+	/*!
+	 * @brief Try to start an asynchronous task without blocking
+	 * @param task Asynchronous task
+	 * @param waiter Pointer of variable of WaitGuard object that state is invalid. If this method succeeds, the WaitGuard object becomes valid. If you don't want to wait for the task finished, specify null pointer or omit this.
+	 * @param priority Thread priority of the asynchronous task
+	 * @retval OK Success. A free thread has been allocated and started
+	 * @retval InvalidParameter \p task is null pointer
+	 * @retval TimedOut Failed to allocate a free thread
+	 *
+	 * @note Same as timedStart(task, Timeout::POLLING, waiter, priority)
+	 */
+	Error tryStart(Runnable* task, WaitGuard* waiter = 0, int priority = Thread::INHERIT_PRIORITY);
+
+	/*!
+	 * @brief Start an asynchronous task with Timeout
+	 *
+	 * If all thread of this ThreadPool have already started, this method blocks the current thread for the limited time until a free thread is allocated.
+	 *
+	 * @param task Asynchronous task
+	 * @param tmout The limited time
+	 * @param waiter Pointer of variable of WaitGuard object that state is invalid. If this method succeeds, the WaitGuard object becomes valid. If you don't want to wait for the task finished, specify null pointer or omit this.
+	 * @param priority Thread priority of the asynchronous task
+	 * @retval OK Success. A free thread has been allocated and started
+	 * @retval InvalidParameter \p task is null pointer
+	 * @retval TimedOut The limited time was elapsed
+	 * @retval CalledByNonThread Called from non thread context (interrupt handler, timer, etc)
+	 *
+	 * @note If \p tmout is Timeout::POLLING then this method does not block.
+	 * @note If \p tmout is Timeout::FOREVER then this method waits forever until a free thread is allocated.
+	 */
+	Error timedStart(Runnable* task, Timeout tmout, WaitGuard* waiter = 0, int priority = Thread::INHERIT_PRIORITY);
+
+	/*!
+	 * @brief Set the UncaughtExceptionHandler for this ThreadPool
+	 * @param handler Pointer of UncaughtExceptionHandler object
+	 */
+	void setUncaughtExceptionHandler(Thread::UncaughtExceptionHandler* handler);
+
+	/*!
+	 * @brief Get the UncaughtExceptionHandler for this ThreadPool
+	 * @return The UncaughtExceptionHandler object
+	 */
+	Thread::UncaughtExceptionHandler* getUncaughtExceptionHandler() const;
+
+private:
+	typedef MessageQueue<TaskRunner*> TaskRunnerMQ;
+	TaskRunnerMQ* m_freeRunnerQueue;
+
+	typedef Container::PreallocatedVector<Thread*> ThreadVector;
+	ThreadVector m_threads;
+	void* m_threadMemory;
+
+	FixedMemoryPool* m_threadMemoryPool;
+	FixedMemoryPool* m_runnerPool;
+	FixedMemoryPool* m_objPool;
+
+	Thread::UncaughtExceptionHandler* m_uncaughtExceptionHandler;
+	const int m_defaultPriority;
+
+	bool constructMembers(std::size_t maxThreads, std::size_t stackSize);
+	void destroyMembers();
+	void destroyRunners();
+	void destroyThreads();
+	void waitThreads();
+	void releaseRunner(TaskRunner* runner);
+	int getDefaultPriority() const { return m_defaultPriority; }
+
+	ThreadPool(FixedMemoryPool* objPool, int defaultPriority);
+	~ThreadPool();
+
+	ThreadPool(const ThreadPool&);
+	ThreadPool& operator=(const ThreadPool&);
+};
+
+}
+
+#endif // OS_WRAPPER_THREAD_POOL_H_INCLUDED
