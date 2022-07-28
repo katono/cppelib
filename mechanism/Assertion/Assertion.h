df64739c3943b3ce49e9c815dd227ea66a39e11c
katono <katono123@gmail.com>
2022-07-28 20:01:57 +0900

    Add Assertion::UserSpecificFunc

--- mechanism/Assertion/Assertion.h
+++ mechanism/Assertion/Assertion.h
@@ -4,12 +4,15 @@
 //! @cond
 #define ASSERTION_ASSERT_STRINGIFY(n) #n
 #define ASSERTION_ASSERT_TOSTRING(n) ASSERTION_ASSERT_STRINGIFY(n)
-//! @endcond
 
 #ifdef NO_EXCEPTIONS
 #include <cstdlib>
-#define CHECK_ASSERT(x) std::abort()
+#define ASSERTION_ASSERTION_FAILURE(x) Assertion::UserSpecificFunc::assertFail(x)
 #else
+#define ASSERTION_ASSERTION_FAILURE(x) throw Assertion::Failure(x)
+#endif
+//! @endcond
+
 /*!
  * @brief Used instead of standard assert() macro for implementing Design by Contract
  * @note This macro is always enabled.
@@ -20,14 +23,13 @@
 #define CHECK_ASSERT(x)\
 	do {\
 		if (!(x)) {\
-			throw Assertion::Failure(__FILE__ "(" ASSERTION_ASSERT_TOSTRING(__LINE__) "): Assertion failed (" #x ")");\
+			ASSERTION_ASSERTION_FAILURE(__FILE__ "(" ASSERTION_ASSERT_TOSTRING(__LINE__) "): Assertion failed (" #x ")");\
 		}\
 	} while (false)
-#endif
 
 
 #ifdef NDEBUG
-#define DEBUG_ASSERT(x)
+#define DEBUG_ASSERT(x)  ((void)0)
 #else
 /*!
  * @brief The same as CHECK_ASSERT() macro
@@ -45,12 +47,93 @@ namespace Assertion {
 class Failure {
 public:
 	explicit Failure(const char* msg) : m_msg(msg) {}
+	/*!
+	 * @brief Get the message when assertion fails
+	 * @return message when assertion fails
+	 */
 	const char* message() const
 	{
 		return m_msg;
 	}
 private:
 	const char* m_msg;
+};
+
+/*!
+ * @brief User specific function class
+ *
+ * If NO_EXCEPTIONS macro is defined, the specified functions are called when CHECK_ASSERT() macro fails.
+ * If NO_EXCEPTIONS macro is not defined, this class do nothing.
+ */
+class UserSpecificFunc {
+public:
+	/*!
+	 * @brief The same function type as std::puts
+	 */
+	typedef int (*PutsType)(const char*);
+
+	/*!
+	 * @brief The same function type as std::abort
+	 */
+	typedef void (*AbortType)();
+
+	/*!
+	 * @brief Set user specific function like puts
+	 * @param func user specific function like puts
+	 *
+	 * When assertion fails, the message is output by func.
+	 * If not called setPuts(), no output.
+	 */
+	static void setPuts(PutsType func)
+	{
+		getPuts() = func;
+	}
+
+	/*!
+	 * @brief Set user specific function like abort
+	 * @param func user specific function like abort
+	 *
+	 * When assertion fails, the application must be aborted by func.
+	 * If not called setAbort() function, aborted by std::abort.
+	 */
+	static void setAbort(AbortType func)
+	{
+		getAbort() = func;
+	}
+
+//! @cond
+private:
+	UserSpecificFunc();
+
+	static PutsType& getPuts()
+	{
+		static PutsType func;
+		return func;
+	}
+
+	static AbortType& getAbort()
+	{
+		static AbortType func;
+		return func;
+	}
+
+#ifdef NO_EXCEPTIONS
+public:
+	static void assertFail(const char* msg)
+	{
+		PutsType& putsFunc = getPuts();
+		if (putsFunc != reinterpret_cast<PutsType>(0)) {
+			putsFunc(msg);
+		}
+		AbortType& abortFunc = getAbort();
+		if (abortFunc != reinterpret_cast<AbortType>(0)) {
+			abortFunc();
+		} else {
+			std::abort();
+		}
+	}
+#endif
+//! @endcond
 };
 
 }
