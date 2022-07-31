30e1a046a7c66fe49caa2914e36f83daba94205b
katono <katono123@gmail.com>
2022-07-31 23:20:19 +0900

    Add AssertionNoExceptionsTest

---
+++ mechanism/test/AssertionNoExceptionsTest/AssertionNoExceptionsTest.cpp
@@ -0,0 +1,53 @@
+#include "Assertion/Assertion.h"
+#include <string>
+#include <csetjmp>
+#include <cstdio>
+
+namespace {
+std::string s_puts;
+std::jmp_buf s_jmpBuf;
+}
+
+int testPuts(const char* str)
+{
+	s_puts = str;
+	// std::puts(str);
+	return 0;
+}
+
+void testAbort()
+{
+	std::longjmp(s_jmpBuf, -1);
+}
+
+int main()
+{
+	Assertion::UserSpecificFunc::setPuts(testPuts);
+	Assertion::UserSpecificFunc::setAbort(testAbort);
+
+	int aborted = 0;
+	if ((aborted = setjmp(s_jmpBuf)) == 0) {
+		bool a = true;
+		CHECK_ASSERT(a);
+	} else if (aborted != 0) {
+		goto testFailed;
+	}
+
+	if ((aborted = setjmp(s_jmpBuf)) == 0) {
+		bool a = false;
+		CHECK_ASSERT(a);
+	} else if (aborted != 0) {
+		if (s_puts.find(__FILE__) == std::string::npos) {
+			goto testFailed;
+		}
+		if (s_puts.find("Assertion failed") == std::string::npos) {
+			goto testFailed;
+		}
+		std::puts("success");
+		return 0;
+	}
+
+testFailed:
+	std::puts("failed");
+	return -1;
+}
