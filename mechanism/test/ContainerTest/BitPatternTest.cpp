20d2cc58d924caf73385ae1829f13a6229bafa6d
katono <katono123@gmail.com>
2022-09-11 20:04:31 +0900

    Fix tests for C++98

--- mechanism/test/ContainerTest/BitPatternTest.cpp
+++ mechanism/test/ContainerTest/BitPatternTest.cpp
@@ -1,6 +1,16 @@
 #include "Container/BitPattern.h"
-#include <cstdint>
+#include <climits>
 #include "CppUTest/TestHarness.h"
+
+// Reason not to use std::uint32_t is to test with C++98.
+#if UINT_MAX == 0xFFFFFFFF
+typedef unsigned int U32;
+#else
+typedef unsigned long U32;
+#if ULONG_MAX != 0xFFFFFFFF
+#define U32_IS_NOT_32BIT
+#endif
+#endif
 
 namespace BitPatternTest {
 
@@ -17,49 +27,53 @@ TEST_GROUP(BitPatternTest) {
 
 TEST(BitPatternTest, default_ctor)
 {
-	BitPattern<std::uint32_t> b;
+	BitPattern<U32> b;
 	UNSIGNED_LONGS_EQUAL(0, b);
 }
 
 TEST(BitPatternTest, ctor)
 {
-	BitPattern<std::uint32_t> b(0x12345678);
+	BitPattern<U32> b(0x12345678);
 	UNSIGNED_LONGS_EQUAL(0x12345678, b);
 }
 
+#ifndef U32_IS_NOT_32BIT
 TEST(BitPatternTest, size)
 {
-	BitPattern<std::uint32_t> b;
+	BitPattern<U32> b;
 	UNSIGNED_LONGS_EQUAL(32, b.size());
 }
+#endif
 
 TEST(BitPatternTest, copy_ctor)
 {
-	BitPattern<std::uint32_t> a(0x12345678);
-	BitPattern<std::uint32_t> b(a);
+	BitPattern<U32> a(0x12345678);
+	BitPattern<U32> b(a);
 	UNSIGNED_LONGS_EQUAL(0x12345678, b);
 }
 
 TEST(BitPatternTest, operator_assign)
 {
-	const BitPattern<std::uint32_t> a(0x12345678);
-	BitPattern<std::uint32_t> b;
+	const BitPattern<U32> a(0x12345678);
+	BitPattern<U32> b;
 	b = a;
 	UNSIGNED_LONGS_EQUAL(0x12345678, b);
 
-	std::uint32_t c = a;
+	U32 c = a;
 	UNSIGNED_LONGS_EQUAL(0x12345678, c);
 
-	BitPattern<std::uint32_t> d;
+	BitPattern<U32> d;
 	d = 0x12345678;
 	UNSIGNED_LONGS_EQUAL(0x12345678, d);
 }
 
 TEST(BitPatternTest, set_reset_all)
 {
-	BitPattern<std::uint32_t> b;
+	BitPattern<U32> b;
 	b.set();
+#ifndef U32_IS_NOT_32BIT
 	UNSIGNED_LONGS_EQUAL(0xFFFFFFFF, b);
+#endif
 
 	b.reset();
 	UNSIGNED_LONGS_EQUAL(0, b);
@@ -67,7 +81,7 @@ TEST(BitPatternTest, set_reset_all)
 
 TEST(BitPatternTest, set_reset_bit)
 {
-	BitPattern<std::uint32_t> b;
+	BitPattern<U32> b;
 	for (std::size_t i = 0; i < 32; i++) {
 		b.set(i);
 		UNSIGNED_LONGS_EQUAL((1U << i), b);
@@ -78,7 +92,7 @@ TEST(BitPatternTest, set_reset_bit)
 
 TEST(BitPatternTest, set_reset_return_this)
 {
-	BitPattern<std::uint32_t> b;
+	BitPattern<U32> b;
 	b.set(0).set(1).set(2).set(3);
 	UNSIGNED_LONGS_EQUAL(0xF, b);
 
@@ -88,14 +102,16 @@ TEST(BitPatternTest, set_reset_return_this)
 
 TEST(BitPatternTest, flip_all)
 {
-	BitPattern<std::uint32_t> b(0x12345678);
+	BitPattern<U32> b(0x12345678);
 	b.flip();
+#ifndef U32_IS_NOT_32BIT
 	UNSIGNED_LONGS_EQUAL(0xEDCBA987, b);
+#endif
 }
 
 TEST(BitPatternTest, flip_bit)
 {
-	BitPattern<std::uint32_t> b;
+	BitPattern<U32> b;
 	for (std::size_t i = 0; i < 32; i++) {
 		b.flip(i);
 		UNSIGNED_LONGS_EQUAL((1U << i), b);
@@ -106,76 +122,78 @@ TEST(BitPatternTest, flip_bit)
 
 TEST(BitPatternTest, flip_return_this)
 {
-	BitPattern<std::uint32_t> b(0x12345678);
+	BitPattern<U32> b(0x12345678);
 	b.flip(0).flip(1).flip(2).flip(3);
 	UNSIGNED_LONGS_EQUAL(0x12345677, b);
 }
 
+#ifndef U32_IS_NOT_32BIT
 TEST(BitPatternTest, operator_tilde)
 {
-	const BitPattern<std::uint32_t> a(0x12345678);
-	BitPattern<std::uint32_t> b = ~a;
+	const BitPattern<U32> a(0x12345678);
+	BitPattern<U32> b = ~a;
 	UNSIGNED_LONGS_EQUAL(0xEDCBA987, b);
 
-	std::uint32_t c = ~a;
+	U32 c = ~a;
 	UNSIGNED_LONGS_EQUAL(0xEDCBA987, c);
 
-	BitPattern<std::uint32_t> d;
+	BitPattern<U32> d;
 	d = ~0x12345678U;
 	UNSIGNED_LONGS_EQUAL(0xEDCBA987, d);
 }
+#endif
 
 TEST(BitPatternTest, operator_and_equal)
 {
-	const BitPattern<std::uint32_t> a(0xAAAAAAAA);
-	BitPattern<std::uint32_t> b(0x82824141);
+	const BitPattern<U32> a(0xAAAAAAAA);
+	BitPattern<U32> b(0x82824141);
 	b &= a;
 	UNSIGNED_LONGS_EQUAL(0x82820000, b);
 
-	std::uint32_t c = 0x82824141;
+	U32 c = 0x82824141;
 	c &= a;
 	UNSIGNED_LONGS_EQUAL(0x82820000, c);
 
-	BitPattern<std::uint32_t> d(0x82824141);
+	BitPattern<U32> d(0x82824141);
 	d &= 0xAAAAAAAA;
 	UNSIGNED_LONGS_EQUAL(0x82820000, d);
 }
 
 TEST(BitPatternTest, operator_or_equal)
 {
-	const BitPattern<std::uint32_t> a(0xAAAAAAAA);
-	BitPattern<std::uint32_t> b(0x82824141);
+	const BitPattern<U32> a(0xAAAAAAAA);
+	BitPattern<U32> b(0x82824141);
 	b |= a;
 	UNSIGNED_LONGS_EQUAL(0xAAAAEBEB, b);
 
-	std::uint32_t c = 0x82824141;
+	U32 c = 0x82824141;
 	c |= a;
 	UNSIGNED_LONGS_EQUAL(0xAAAAEBEB, c);
 
-	BitPattern<std::uint32_t> d(0x82824141);
+	BitPattern<U32> d(0x82824141);
 	d |= 0xAAAAAAAA;
 	UNSIGNED_LONGS_EQUAL(0xAAAAEBEB, d);
 }
 
 TEST(BitPatternTest, operator_xor_equal)
 {
-	const BitPattern<std::uint32_t> a(0xAAAAAAAA);
-	BitPattern<std::uint32_t> b(0x82824141);
+	const BitPattern<U32> a(0xAAAAAAAA);
+	BitPattern<U32> b(0x82824141);
 	b ^= a;
 	UNSIGNED_LONGS_EQUAL(0x2828EBEB, b);
 
-	std::uint32_t c = 0x82824141;
+	U32 c = 0x82824141;
 	c ^= a;
 	UNSIGNED_LONGS_EQUAL(0x2828EBEB, c);
 
-	BitPattern<std::uint32_t> d(0x82824141);
+	BitPattern<U32> d(0x82824141);
 	d ^= 0xAAAAAAAA;
 	UNSIGNED_LONGS_EQUAL(0x2828EBEB, d);
 }
 
 TEST(BitPatternTest, operator_left_right_shift_equal)
 {
-	BitPattern<std::uint32_t> b(0x1);
+	BitPattern<U32> b(0x1);
 	for (std::size_t i = 0; i < 32; i++) {
 		b <<= i;
 		UNSIGNED_LONGS_EQUAL((1U << i), b);
@@ -186,19 +204,19 @@ TEST(BitPatternTest, operator_left_right_shift_equal)
 
 TEST(BitPatternTest, operator_left_right_shift)
 {
-	BitPattern<std::uint32_t> a(0x1);
+	BitPattern<U32> a(0x1);
 	for (std::size_t i = 0; i < 32; i++) {
-		BitPattern<std::uint32_t> b = a << i;
+		BitPattern<U32> b = a << i;
 		UNSIGNED_LONGS_EQUAL((1U << i), b);
-		BitPattern<std::uint32_t> c = b >> i;
+		BitPattern<U32> c = b >> i;
 		UNSIGNED_LONGS_EQUAL(1, c);
 	}
 }
 
 TEST(BitPatternTest, operator_equal_true)
 {
-	const BitPattern<std::uint32_t> a(0x12345678);
-	BitPattern<std::uint32_t> b(0x12345678);
+	const BitPattern<U32> a(0x12345678);
+	BitPattern<U32> b(0x12345678);
 	bool f = (a == b);
 	CHECK(f);
 
@@ -208,8 +226,8 @@ TEST(BitPatternTest, operator_equal_true)
 
 TEST(BitPatternTest, operator_equal_false)
 {
-	const BitPattern<std::uint32_t> a(0x12345678);
-	BitPattern<std::uint32_t> b(0x12345679);
+	const BitPattern<U32> a(0x12345678);
+	BitPattern<U32> b(0x12345679);
 	bool f = (a == b);
 	CHECK_FALSE(f);
 
@@ -219,8 +237,8 @@ TEST(BitPatternTest, operator_equal_false)
 
 TEST(BitPatternTest, operator_not_equal_true)
 {
-	const BitPattern<std::uint32_t> a(0x12345678);
-	BitPattern<std::uint32_t> b(0x12345679);
+	const BitPattern<U32> a(0x12345678);
+	BitPattern<U32> b(0x12345679);
 	bool f = (a != b);
 	CHECK(f);
 
@@ -230,8 +248,8 @@ TEST(BitPatternTest, operator_not_equal_true)
 
 TEST(BitPatternTest, operator_not_equal_false)
 {
-	const BitPattern<std::uint32_t> a(0x12345678);
-	BitPattern<std::uint32_t> b(0x12345678);
+	const BitPattern<U32> a(0x12345678);
+	BitPattern<U32> b(0x12345678);
 	bool f = (a != b);
 	CHECK_FALSE(f);
 
@@ -241,104 +259,107 @@ TEST(BitPatternTest, operator_not_equal_false)
 
 TEST(BitPatternTest, test)
 {
-	BitPattern<std::uint32_t> a(0xAAAAAAAA);
+	BitPattern<U32> a(0xAAAAAAAA);
 	CHECK_FALSE(a.test(0));
 	CHECK(a.test(1));
 	CHECK_FALSE(a.test(2));
 	CHECK(a.test(3));
 }
 
+#ifndef U32_IS_NOT_32BIT
 TEST(BitPatternTest, all_true)
 {
-	BitPattern<std::uint32_t> a(0xFFFFFFFF);
+	BitPattern<U32> a(0xFFFFFFFF);
 	CHECK(a.all());
 }
+#endif
 
 TEST(BitPatternTest, all_false)
 {
-	BitPattern<std::uint32_t> a(0xFFFFFFFE);
+	BitPattern<U32> a(0xFFFFFFFE);
 	CHECK_FALSE(a.all());
 }
 
 TEST(BitPatternTest, any_true)
 {
-	BitPattern<std::uint32_t> a(0x1);
+	BitPattern<U32> a(0x1);
 	CHECK(a.any());
 }
 
 TEST(BitPatternTest, any_false)
 {
-	BitPattern<std::uint32_t> a(0x0);
+	BitPattern<U32> a(0x0);
 	CHECK_FALSE(a.any());
 }
 
 TEST(BitPatternTest, none_true)
 {
-	BitPattern<std::uint32_t> a(0x0);
+	BitPattern<U32> a(0x0);
 	CHECK(a.none());
 }
 
 TEST(BitPatternTest, none_false)
 {
-	BitPattern<std::uint32_t> a(0x1);
+	BitPattern<U32> a(0x1);
 	CHECK_FALSE(a.none());
 }
 
 TEST(BitPatternTest, operator_and)
 {
-	const BitPattern<std::uint32_t> a(0xAAAAAAAA);
-	BitPattern<std::uint32_t> b(0x82824141);
-	BitPattern<std::uint32_t> c = b & a;
+	const BitPattern<U32> a(0xAAAAAAAA);
+	BitPattern<U32> b(0x82824141);
+	BitPattern<U32> c = b & a;
 	UNSIGNED_LONGS_EQUAL(0x82820000, c);
 
-	std::uint32_t d = b & a;
+	U32 d = b & a;
 	UNSIGNED_LONGS_EQUAL(0x82820000, d);
 
-	BitPattern<std::uint32_t> e = b & 0xAAAAAAAA;
+	BitPattern<U32> e = b & 0xAAAAAAAA;
 	UNSIGNED_LONGS_EQUAL(0x82820000, e);
 
-	std::uint32_t f = b & 0xAAAAAAAA;
+	U32 f = b & 0xAAAAAAAA;
 	UNSIGNED_LONGS_EQUAL(0x82820000, f);
 }
 
 TEST(BitPatternTest, operator_or)
 {
-	const BitPattern<std::uint32_t> a(0xAAAAAAAA);
-	BitPattern<std::uint32_t> b(0x82824141);
-	BitPattern<std::uint32_t> c = b | a;
+	const BitPattern<U32> a(0xAAAAAAAA);
+	BitPattern<U32> b(0x82824141);
+	BitPattern<U32> c = b | a;
 	UNSIGNED_LONGS_EQUAL(0xAAAAEBEB, c);
 
-	std::uint32_t d = b | a;
+	U32 d = b | a;
 	UNSIGNED_LONGS_EQUAL(0xAAAAEBEB, d);
 
-	BitPattern<std::uint32_t> e = b | 0xAAAAAAAA;
+	BitPattern<U32> e = b | 0xAAAAAAAA;
 	UNSIGNED_LONGS_EQUAL(0xAAAAEBEB, e);
 
-	std::uint32_t f = b | 0xAAAAAAAA;
+	U32 f = b | 0xAAAAAAAA;
 	UNSIGNED_LONGS_EQUAL(0xAAAAEBEB, f);
 }
 
 TEST(BitPatternTest, operator_xor)
 {
-	const BitPattern<std::uint32_t> a(0xAAAAAAAA);
-	BitPattern<std::uint32_t> b(0x82824141);
-	BitPattern<std::uint32_t> c = b ^ a;
+	const BitPattern<U32> a(0xAAAAAAAA);
+	BitPattern<U32> b(0x82824141);
+	BitPattern<U32> c = b ^ a;
 	UNSIGNED_LONGS_EQUAL(0x2828EBEB, c);
 
-	std::uint32_t d = b ^ a;
+	U32 d = b ^ a;
 	UNSIGNED_LONGS_EQUAL(0x2828EBEB, d);
 
-	BitPattern<std::uint32_t> e = b ^ 0xAAAAAAAA;
+	BitPattern<U32> e = b ^ 0xAAAAAAAA;
 	UNSIGNED_LONGS_EQUAL(0x2828EBEB, e);
 
-	std::uint32_t f = b ^ 0xAAAAAAAA;
+	U32 f = b ^ 0xAAAAAAAA;
 	UNSIGNED_LONGS_EQUAL(0x2828EBEB, f);
 }
 
 #ifndef NDEBUG
+#ifndef U32_IS_NOT_32BIT
 TEST(BitPatternTest, set_precondition_failed)
 {
-	BitPattern<std::uint32_t> b;
+	BitPattern<U32> b;
 	try {
 		b.set(32);
 	}
@@ -351,7 +372,7 @@ TEST(BitPatternTest, set_precondition_failed)
 
 TEST(BitPatternTest, reset_precondition_failed)
 {
-	BitPattern<std::uint32_t> b;
+	BitPattern<U32> b;
 	try {
 		b.reset(32);
 	}
@@ -364,7 +385,7 @@ TEST(BitPatternTest, reset_precondition_failed)
 
 TEST(BitPatternTest, flip_precondition_failed)
 {
-	BitPattern<std::uint32_t> b;
+	BitPattern<U32> b;
 	try {
 		b.flip(32);
 	}
@@ -377,7 +398,7 @@ TEST(BitPatternTest, flip_precondition_failed)
 
 TEST(BitPatternTest, test_precondition_failed)
 {
-	BitPattern<std::uint32_t> b;
+	BitPattern<U32> b;
 	try {
 		b.test(32);
 	}
@@ -387,6 +408,7 @@ TEST(BitPatternTest, test_precondition_failed)
 	}
 	FAIL("failed");
 }
+#endif
 #endif
 
 TEST(BitPatternTest, test_8bit)
@@ -443,6 +465,7 @@ TEST(BitPatternTest, test_16bit)
 	CHECK_FALSE(b.test(0));
 }
 
+#if (__cplusplus >= 201103L) || !defined(CPPELIB_NO_LONG_LONG)
 TEST(BitPatternTest, test_64bit)
 {
 	BitPattern<unsigned long long> b;
@@ -469,5 +492,6 @@ TEST(BitPatternTest, test_64bit)
 	CHECK(b.test(63));
 	CHECK_FALSE(b.test(0));
 }
+#endif
 
 } // namespace BitPatternTest
