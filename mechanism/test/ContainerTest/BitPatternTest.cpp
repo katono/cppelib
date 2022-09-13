b467714a1af01d04f49e6c42e0b7ca5aedc4bd7a
katono <katono123@gmail.com>
2022-09-13 20:38:05 +0900

    Add testcase to detect bug

--- mechanism/test/ContainerTest/BitPatternTest.cpp
+++ mechanism/test/ContainerTest/BitPatternTest.cpp
@@ -389,4 +389,85 @@ TEST(BitPatternTest, test_precondition_failed)
 }
 #endif
 
+TEST(BitPatternTest, test_8bit)
+{
+	BitPattern<unsigned char> b;
+	UNSIGNED_LONGS_EQUAL(8, b.size());
+	UNSIGNED_LONGS_EQUAL(0x0, b);
+
+	b.set(0).set(1).set(2).set(3);
+	UNSIGNED_LONGS_EQUAL(0xF, b);
+	CHECK_FALSE(b.all());
+	b.set();
+	UNSIGNED_LONGS_EQUAL(0xFF, b);
+	CHECK(b.all());
+
+	b.reset(0).reset(1).reset(2).reset(3);
+	UNSIGNED_LONGS_EQUAL(0xF0, b);
+	b.reset();
+	UNSIGNED_LONGS_EQUAL(0x0, b);
+
+	b.flip(0).flip(1).flip(2).flip(3);
+	UNSIGNED_LONGS_EQUAL(0xF, b);
+	b.flip();
+	UNSIGNED_LONGS_EQUAL(0xF0, b);
+
+	CHECK(b.test(7));
+	CHECK_FALSE(b.test(0));
+}
+
+TEST(BitPatternTest, test_16bit)
+{
+	BitPattern<unsigned short> b;
+	UNSIGNED_LONGS_EQUAL(16, b.size());
+	UNSIGNED_LONGS_EQUAL(0x0, b);
+
+	b.set(0).set(1).set(2).set(3);
+	UNSIGNED_LONGS_EQUAL(0xF, b);
+	CHECK_FALSE(b.all());
+	b.set();
+	UNSIGNED_LONGS_EQUAL(0xFFFF, b);
+	CHECK(b.all());
+
+	b.reset(0).reset(1).reset(2).reset(3);
+	UNSIGNED_LONGS_EQUAL(0xFFF0, b);
+	b.reset();
+	UNSIGNED_LONGS_EQUAL(0x0, b);
+
+	b.flip(0).flip(1).flip(2).flip(3);
+	UNSIGNED_LONGS_EQUAL(0xF, b);
+	b.flip();
+	UNSIGNED_LONGS_EQUAL(0xFFF0, b);
+
+	CHECK(b.test(15));
+	CHECK_FALSE(b.test(0));
+}
+
+TEST(BitPatternTest, test_64bit)
+{
+	BitPattern<unsigned long long> b;
+	UNSIGNED_LONGS_EQUAL(64, b.size());
+	UNSIGNED_LONGLONGS_EQUAL(0x0, b);
+
+	b.set(0).set(1).set(2).set(3);
+	UNSIGNED_LONGLONGS_EQUAL(0xF, b);
+	CHECK_FALSE(b.all());
+	b.set();
+	UNSIGNED_LONGLONGS_EQUAL(0xFFFFFFFFFFFFFFFF, b);
+	CHECK(b.all());
+
+	b.reset(0).reset(1).reset(2).reset(3);
+	UNSIGNED_LONGLONGS_EQUAL(0xFFFFFFFFFFFFFFF0, b);
+	b.reset();
+	UNSIGNED_LONGLONGS_EQUAL(0x0, b);
+
+	b.flip(0).flip(1).flip(2).flip(3);
+	UNSIGNED_LONGLONGS_EQUAL(0xF, b);
+	b.flip();
+	UNSIGNED_LONGLONGS_EQUAL(0xFFFFFFFFFFFFFFF0, b);
+
+	CHECK(b.test(63));
+	CHECK_FALSE(b.test(0));
+}
+
 } // namespace BitPatternTest
