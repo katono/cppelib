3593f74b68880a2898853448e8f5129f40a8c2ab
katono <katono123@gmail.com>
2023-06-30 23:23:17 +0900

    Add CMakeLists.txt, conanfile.py for mechanism

---
+++ mechanism/conanfile.py
@@ -0,0 +1,52 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+
+
+class cppelib_mechanismRecipe(ConanFile):
+    name = "cppelib_mechanism"
+    version = "1.5.0"
+
+    # Optional metadata
+    license = "MIT License"
+    author = "KATO Noriaki <katono123@gmail.com>"
+    url = "https://github.com/katono/cppelib"
+    description = "Portable C++ library for embedded software development"
+    topics = ("embedded", "rtos")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "Assertion/*", "Container/*", "OSWrapper/*"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.rm_safe("fPIC")
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["cppelib_mechanism"]
+
