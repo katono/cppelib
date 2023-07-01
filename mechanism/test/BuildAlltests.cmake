ed555a7093293ca02c3aaae9ec92f1cb8b1814c2
katono <katono123@gmail.com>
2023-07-01 12:16:34 +0900

    Add conanfile.py for test

--- mechanism/test/BuildAlltests.cmake
+++ mechanism/test/BuildAlltests.cmake
@@ -1,51 +1,32 @@
-include_directories(${CPPUTEST_DIR}/include)
-include_directories(${MECHANISM_DIR})
-
-link_directories(${CPPUTEST_BUILD_DIR}/src/CppUTest)
-link_directories(${CPPUTEST_BUILD_DIR}/src/CppUTestExt)
-
 set(AssertionTestSrc
-	${MECHANISM_DIR}/test/AssertionTest/AssertionTest.cpp
+	${TEST_DIR}/AssertionTest/AssertionTest.cpp
 )
 
 set(ContainerTestSrc
-	${MECHANISM_DIR}/test/ContainerTest/ArrayTest.cpp
-	${MECHANISM_DIR}/test/ContainerTest/BitPatternTest.cpp
-	${MECHANISM_DIR}/test/ContainerTest/FixedDequeTest.cpp
-	${MECHANISM_DIR}/test/ContainerTest/FixedVectorTest.cpp
-	${MECHANISM_DIR}/test/ContainerTest/IntrusiveListTest.cpp
-	${MECHANISM_DIR}/test/ContainerTest/PreallocatedDequeTest.cpp
-	${MECHANISM_DIR}/test/ContainerTest/PreallocatedVectorTest.cpp
-)
-
-set(OSWrapperSrc
-	${MECHANISM_DIR}/OSWrapper/EventFlag.cpp
-	${MECHANISM_DIR}/OSWrapper/FixedMemoryPool.cpp
-	${MECHANISM_DIR}/OSWrapper/Mutex.cpp
-	${MECHANISM_DIR}/OSWrapper/OneShotTimer.cpp
-	${MECHANISM_DIR}/OSWrapper/PeriodicTimer.cpp
-	${MECHANISM_DIR}/OSWrapper/Thread.cpp
-	${MECHANISM_DIR}/OSWrapper/Timeout.cpp
-	${MECHANISM_DIR}/OSWrapper/VariableMemoryPool.cpp
-	${MECHANISM_DIR}/OSWrapper/ThreadPool.cpp
+	${TEST_DIR}/ContainerTest/ArrayTest.cpp
+	${TEST_DIR}/ContainerTest/BitPatternTest.cpp
+	${TEST_DIR}/ContainerTest/FixedDequeTest.cpp
+	${TEST_DIR}/ContainerTest/FixedVectorTest.cpp
+	${TEST_DIR}/ContainerTest/IntrusiveListTest.cpp
+	${TEST_DIR}/ContainerTest/PreallocatedDequeTest.cpp
+	${TEST_DIR}/ContainerTest/PreallocatedVectorTest.cpp
 )
 
 set(OSWrapperTestSrc
-	${MECHANISM_DIR}/test/OSWrapperTest/EventFlagTest.cpp
-	${MECHANISM_DIR}/test/OSWrapperTest/FixedMemoryPoolTest.cpp
-	${MECHANISM_DIR}/test/OSWrapperTest/MutexTest.cpp
-	${MECHANISM_DIR}/test/OSWrapperTest/OneShotTimerTest.cpp
-	${MECHANISM_DIR}/test/OSWrapperTest/PeriodicTimerTest.cpp
-	${MECHANISM_DIR}/test/OSWrapperTest/ThreadTest.cpp
-	${MECHANISM_DIR}/test/OSWrapperTest/VariableMemoryPoolTest.cpp
-	${MECHANISM_DIR}/test/OSWrapperTest/MessageQueueTest.cpp
-	${MECHANISM_DIR}/test/OSWrapperTest/ThreadPoolTest.cpp
+	${TEST_DIR}/OSWrapperTest/EventFlagTest.cpp
+	${TEST_DIR}/OSWrapperTest/FixedMemoryPoolTest.cpp
+	${TEST_DIR}/OSWrapperTest/MutexTest.cpp
+	${TEST_DIR}/OSWrapperTest/OneShotTimerTest.cpp
+	${TEST_DIR}/OSWrapperTest/PeriodicTimerTest.cpp
+	${TEST_DIR}/OSWrapperTest/ThreadTest.cpp
+	${TEST_DIR}/OSWrapperTest/VariableMemoryPoolTest.cpp
+	${TEST_DIR}/OSWrapperTest/MessageQueueTest.cpp
+	${TEST_DIR}/OSWrapperTest/ThreadPoolTest.cpp
 )
 
-add_executable(alltests
-	${MECHANISM_DIR}/test/main.cpp
+add_executable(${PROJECT_NAME}
+	${TEST_DIR}/main.cpp
 	${AssertionTestSrc}
 	${ContainerTestSrc}
-	${OSWrapperSrc}
 	${OSWrapperTestSrc}
 )
---
+++ mechanism/test/cmake_Cpp11Test/conanfile.py
@@ -0,0 +1,36 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+
+
+class runTestsRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    def requirements(self):
+        self.requires("cpputest/4.0")
+        self.requires("cppelib_mechanism/1.5.0")
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.rm_safe("fPIC")
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.test(target="test_verbose")
+
