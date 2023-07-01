5c7d9da822bee6d755534120956be4b6ac393644
katono <katono123@gmail.com>
2023-07-01 21:30:54 +0900

    Add AssertionNoExceptionsTest/conanfile.py

---
+++ mechanism/test/AssertionNoExceptionsTest/conanfile.py
@@ -0,0 +1,33 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+
+
+class runTestsRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.rm_safe("fPIC")
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cppstd = str(self.settings.compiler.cppstd).replace("gnu", "")
+        cmake.configure(cli_args=[f"-DCMAKE_CXX_STANDARD={cppstd}"])
+        cmake.build()
+        cmake.test()
+
