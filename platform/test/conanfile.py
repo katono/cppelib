4fb9efd31ddf87778d3fed5b1fda48c328a20201
katono <katono123@gmail.com>
2023-07-02 22:11:09 +0900

    Add platform/test/conanfile.py

---
+++ platform/test/conanfile.py
@@ -0,0 +1,41 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+
+
+class runTestsRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    def requirements(self):
+        self.requires("cpputest/4.0")
+        self.requires("cppelib_mechanism/1.5.0")
+        self.requires("cppelib_platform/1.5.0")
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.rm_safe("fPIC")
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        compiler = self.settings.compiler
+        compiler_version = compiler.version
+        arch = self.settings.arch
+        cppstd = compiler.cppstd
+        cmake_layout(self, build_folder=f"build/{compiler}-{compiler_version}-{arch}-{cppstd}")
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.test()
+
