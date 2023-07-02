9e088f03027c0a8a26ab8daf24fbe25b9e442d33
katono <katono123@gmail.com>
2023-07-02 13:35:14 +0900

    Fix test_package

--- platform/test_package/src/example.cpp
+++ platform/test_package/src/example.cpp
@@ -1,7 +1,35 @@
 #include "OSWrapper/Thread.h"
 
+#if defined(PLATFORM_OS_WINDOWS)
+#include "WindowsOSWrapper/WindowsOSWrapper.h"
+#elif defined(PLATFORM_OS_POSIX)
+#include "PosixOSWrapper/PosixOSWrapper.h"
+#endif
+#include "OSWrapper/Runnable.h"
+#include "OSWrapper/Thread.h"
+#include <cstdio>
+
+class TestRunnable : public OSWrapper::Runnable {
+public:
+	void run()
+	{
+		std::printf("Thread running\n");
+	}
+};
+
+
 int main()
 {
-	OSWrapper::registerThreadFactory(0);
+#if defined(PLATFORM_OS_WINDOWS)
+	WindowsOSWrapper::init(1, 9);
+#elif defined(PLATFORM_OS_POSIX)
+	PosixOSWrapper::init(1, 9);
+#endif
+	TestRunnable r;
+	OSWrapper::Thread* t = OSWrapper::Thread::create(&r);
+	t->start();
+	t->wait();
+	std::printf("Thread finished\n");
+	OSWrapper::Thread::destroy(t);
 	return 0;
 }
