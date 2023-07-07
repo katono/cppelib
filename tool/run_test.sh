a6263d6faff49dc7b1c271be4024919fec49bfc1
katono <katono123@gmail.com>
2023-07-07 23:38:55 +0900

    Fix test script

--- tool/run_test.sh
+++ tool/run_test.sh
@@ -11,41 +11,20 @@ cd ${REPO_ROOT}
 EXIT_CODE=0
 
 BUILD_DIR=build
-BUILD_DIRS=${BUILD_DIRS}" mechanism/test/cmake_Cpp98Test/${BUILD_DIR}"
-BUILD_DIRS=${BUILD_DIRS}" mechanism/test/cmake_Cpp11Test/${BUILD_DIR}"
-BUILD_DIRS=${BUILD_DIRS}" mechanism/test/AssertionNoExceptionsTest/${BUILD_DIR}"
-BUILD_DIRS=${BUILD_DIRS}" mechanism/test/ContainerNoExceptionsTest/${BUILD_DIR}"
-BUILD_DIRS=${BUILD_DIRS}" platform/test/cmake_StdCppOSWrapperTest/${BUILD_DIR}"
-BUILD_DIRS=${BUILD_DIRS}" platform/test/cmake_StdCppOSWrapperNoExceptionsTest/${BUILD_DIR}"
-BUILD_DIRS=${BUILD_DIRS}" platform/test/cmake_PosixOSWrapperTest/${BUILD_DIR}"
-for dir in ${BUILD_DIRS}; do
+TEST_DIRS=${TEST_DIRS}" mechanism/test"
+TEST_DIRS=${TEST_DIRS}" platform/test"
+for dir in ${TEST_DIRS}; do
 	if [ "$2" = "clean" ]; then
-		rm -rf ${dir}
+		rm -rf ${dir}/${BUILD_DIR}
 	else
 		if [ "$2" = "rebuild" ]; then
-			rm -rf ${dir}
+			rm -rf ${dir}/${BUILD_DIR}
 		fi
-		mkdir -p ${dir}
-		cd ${dir}
-		cmake ..
+		conan build ${dir} --build=missing
 		if [ "$?" != "0" ]; then
 			EXIT_CODE=1
-			cd -
 			continue
 		fi
-		make
-		if [ "$?" != "0" ]; then
-			EXIT_CODE=1
-			cd -
-			continue
-		fi
-		ctest -V
-		if [ "$?" != "0" ]; then
-			EXIT_CODE=1
-			cd -
-			continue
-		fi
-		cd -
 	fi
 done
 
