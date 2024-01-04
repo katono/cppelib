4f350bcaedffd191b32dd1d4398971186376de49
katono <katono123@gmail.com>
2024-01-04 19:52:38 +0900

    Fix run_test.sh to use profiles

--- tool/run_test.sh
+++ tool/run_test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "$1" = "" ]; then
 	echo "Usage: $0 REPO_ROOT"
@@ -12,19 +12,25 @@ EXIT_CODE=0
 
 TEST_DIRS=${TEST_DIRS}" mechanism/test"
 TEST_DIRS=${TEST_DIRS}" platform/test"
-STD_CPP="11 17"
+PROFILES=${PROFILES}" tool/profile_cppstd11"
+PROFILES=${PROFILES}" tool/profile_cppstd11_noexceptions"
+PROFILES=${PROFILES}" tool/profile_cppstd17"
+PROFILES=${PROFILES}" tool/profile_cppstd17_noexceptions"
+PROFILES=${PROFILES}" tool/profile_cppstd98"
+PROFILES=${PROFILES}" tool/profile_cppstd98_noexceptions"
+BUILD_TYPE="Debug Release"
 for dir in ${TEST_DIRS}; do
-	for std in ${STD_CPP}; do
-		conan build ${dir} --build=missing -s compiler.cppstd=${std}
-		if [ "$?" != "0" ]; then
-			EXIT_CODE=1
+	for pr in ${PROFILES}; do
+		if [[ ${dir} =~ platform ]] && [[ ${pr} =~ cppstd98 ]]; then
+			continue
 		fi
+		for type in ${BUILD_TYPE}; do
+			conan build ${dir} --build=missing -pr ${pr} -s build_type=${type}
+			if [ "$?" != "0" ]; then
+				EXIT_CODE=1
+			fi
+		done
 	done
 done
-
-conan build mechanism/test --build=missing -s compiler.cppstd=98
-if [ "$?" != "0" ]; then
-	EXIT_CODE=1
-fi
 
 exit ${EXIT_CODE}
