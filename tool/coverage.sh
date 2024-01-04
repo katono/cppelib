5d17515f690d22932a94f23a3e84c472c589df89
katono <katono123@gmail.com>
2024-01-04 20:59:45 +0900

    Fix coverage.sh to use profiles

--- tool/coverage.sh
+++ tool/coverage.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "$1" = "" ]; then
 	echo "Usage: $0 REPO_ROOT"
@@ -15,20 +15,24 @@ EXIT_CODE=0
 
 TEST_DIRS=${TEST_DIRS}" mechanism/test"
 TEST_DIRS=${TEST_DIRS}" platform/test"
+PROFILES=${PROFILES}" tool/profile_cppstd11_coverage"
+PROFILES=${PROFILES}" tool/profile_cppstd11_coverage_noexceptions"
+PROFILES=${PROFILES}" tool/profile_cppstd17_coverage"
+PROFILES=${PROFILES}" tool/profile_cppstd17_coverage_noexceptions"
+PROFILES=${PROFILES}" tool/profile_cppstd98_coverage"
+PROFILES=${PROFILES}" tool/profile_cppstd98_coverage_noexceptions"
 STD_CPP="11 17"
 for dir in ${TEST_DIRS}; do
-	for std in ${STD_CPP}; do
-		conan build ${dir} --build=missing -s compiler.cppstd=${std} -c tools.build:cxxflags+="--coverage" -c tools.build:exelinkflags+="--coverage" -of ${COVERAGE_BUILD_DIR}/${dir} -s build_type=Debug
+	for pr in ${PROFILES}; do
+		if [[ ${dir} =~ platform ]] && [[ ${pr} =~ cppstd98 ]]; then
+			continue
+		fi
+		conan build ${dir} --build=missing -pr ${pr} -of ${COVERAGE_BUILD_DIR}/${dir} -s build_type=Debug
 		if [ "$?" != "0" ]; then
 			EXIT_CODE=1
 		fi
 	done
 done
-
-conan build mechanism/test --build=missing -s compiler.cppstd=98 -c tools.build:cxxflags+="--coverage" -c tools.build:exelinkflags+="--coverage" -of ${COVERAGE_BUILD_DIR}/${dir} -s build_type=Debug
-if [ "$?" != "0" ]; then
-	EXIT_CODE=1
-fi
 
 lcov -d ${COVERAGE_BUILD_DIR} -c -o ${COVERAGE_BUILD_DIR}/coverage.info
 lcov -r ${COVERAGE_BUILD_DIR}/coverage.info "*/CppUTest*/*" "/usr/*" "*/test/*" -o ${COVERAGE_BUILD_DIR}/coverage.info
