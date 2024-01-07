4f5cb9238cfc086bdeffc3987953f25542aea968
katono <katono123@gmail.com>
2024-01-07 21:18:57 +0900

    Fix to add TestHelper::destroy* method

--- platform/test/PlatformOSWrapperTest/PlatformOSWrapperTestHelper.h
+++ platform/test/PlatformOSWrapperTest/PlatformOSWrapperTestHelper.h
@@ -20,6 +20,7 @@ OSWrapper::ThreadFactory* getThreadFactory();
 class TestHelper {
 public:
 	virtual ~TestHelper() {}
+
 	virtual OSWrapper::ThreadFactory* createThreadFactory() = 0;
 	virtual OSWrapper::MutexFactory* createMutexFactory() = 0;
 	virtual OSWrapper::EventFlagFactory* createEventFlagFactory() = 0;
@@ -27,6 +28,15 @@ public:
 	virtual OSWrapper::VariableMemoryPoolFactory* createVariableMemoryPoolFactory() = 0;
 	virtual OSWrapper::PeriodicTimerFactory* createPeriodicTimerFactory() = 0;
 	virtual OSWrapper::OneShotTimerFactory* createOneShotTimerFactory() = 0;
+
+	virtual void destroyThreadFactory(OSWrapper::ThreadFactory* factory) = 0;
+	virtual void destroyMutexFactory(OSWrapper::MutexFactory* factory) = 0;
+	virtual void destroyEventFlagFactory(OSWrapper::EventFlagFactory* factory) = 0;
+	virtual void destroyFixedMemoryPoolFactory(OSWrapper::FixedMemoryPoolFactory* factory) = 0;
+	virtual void destroyVariableMemoryPoolFactory(OSWrapper::VariableMemoryPoolFactory* factory) = 0;
+	virtual void destroyPeriodicTimerFactory(OSWrapper::PeriodicTimerFactory* factory) = 0;
+	virtual void destroyOneShotTimerFactory(OSWrapper::OneShotTimerFactory* factory) = 0;
+
 	virtual unsigned long getCurrentTime() = 0;
 };
 
