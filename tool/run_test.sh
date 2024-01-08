24d4000c40417838820a01c0d17b86cb373dcb55
katono <katono123@gmail.com>
2024-01-08 22:48:18 +0900

    Fix to test with -o platform_os=XXX option

--- tool/run_test.sh
+++ tool/run_test.sh
@@ -10,22 +10,29 @@ cd ${REPO_ROOT}
 
 EXIT_CODE=0
 
-TEST_DIRS=${TEST_DIRS}" mechanism/test"
-TEST_DIRS=${TEST_DIRS}" platform/test"
 PROFILES=${PROFILES}" tool/profiles/profile_cppstd11"
 PROFILES=${PROFILES}" tool/profiles/profile_cppstd11_noexceptions"
 PROFILES=${PROFILES}" tool/profiles/profile_cppstd17"
 PROFILES=${PROFILES}" tool/profiles/profile_cppstd17_noexceptions"
-PROFILES=${PROFILES}" tool/profiles/profile_cppstd98"
-PROFILES=${PROFILES}" tool/profiles/profile_cppstd98_noexceptions"
+PROFILES_STD98=${PROFILES_STD98}" tool/profiles/profile_cppstd98"
+PROFILES_STD98=${PROFILES_STD98}" tool/profiles/profile_cppstd98_noexceptions"
 BUILD_TYPE="Debug Release"
-for dir in ${TEST_DIRS}; do
-	for pr in ${PROFILES}; do
-		if [[ ${dir} =~ platform ]] && [[ ${pr} =~ cppstd98 ]]; then
-			continue
+
+for pr in ${PROFILES} ${PROFILES_STD98}; do
+	for type in ${BUILD_TYPE}; do
+		conan build mechanism/test --build=missing -pr ${pr} -s build_type=${type}
+		if [ "$?" != "0" ]; then
+			EXIT_CODE=1
 		fi
-		for type in ${BUILD_TYPE}; do
-			conan build ${dir} --build=missing -pr ${pr} -s build_type=${type}
+	done
+done
+
+PLATFORM_OS=${PLATFORM_OS}" POSIX"
+PLATFORM_OS=${PLATFORM_OS}" STDCPP"
+for pr in ${PROFILES}; do
+	for type in ${BUILD_TYPE}; do
+		for platform in ${PLATFORM_OS}; do
+			conan build platform/test --build=missing -pr ${pr} -s build_type=${type} -o platform_os=${platform}
 			if [ "$?" != "0" ]; then
 				EXIT_CODE=1
 			fi
