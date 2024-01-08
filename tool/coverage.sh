fd89cdf6c1d4072becee86c20183e655b1c4b073
katono <katono123@gmail.com>
2024-01-08 22:56:14 +0900

    Fix to test with -o platform_os=XXX option

--- tool/coverage.sh
+++ tool/coverage.sh
@@ -13,21 +13,25 @@ rm -rf ${COVERAGE_BUILD_DIR}
 
 EXIT_CODE=0
 
-TEST_DIRS=${TEST_DIRS}" mechanism/test"
-TEST_DIRS=${TEST_DIRS}" platform/test"
 PROFILES=${PROFILES}" tool/profiles/profile_cppstd11_coverage"
 PROFILES=${PROFILES}" tool/profiles/profile_cppstd11_coverage_noexceptions"
 PROFILES=${PROFILES}" tool/profiles/profile_cppstd17_coverage"
 PROFILES=${PROFILES}" tool/profiles/profile_cppstd17_coverage_noexceptions"
-PROFILES=${PROFILES}" tool/profiles/profile_cppstd98_coverage"
-PROFILES=${PROFILES}" tool/profiles/profile_cppstd98_coverage_noexceptions"
-STD_CPP="11 17"
-for dir in ${TEST_DIRS}; do
-	for pr in ${PROFILES}; do
-		if [[ ${dir} =~ platform ]] && [[ ${pr} =~ cppstd98 ]]; then
-			continue
-		fi
-		conan build ${dir} --build=missing -pr ${pr} -of ${COVERAGE_BUILD_DIR}/${dir} -s build_type=Debug
+PROFILES_STD98=${PROFILES_STD98}" tool/profiles/profile_cppstd98_coverage"
+PROFILES_STD98=${PROFILES_STD98}" tool/profiles/profile_cppstd98_coverage_noexceptions"
+
+for pr in ${PROFILES} ${PROFILES_STD98}; do
+	conan build mechanism/test --build=missing -pr ${pr} -of ${COVERAGE_BUILD_DIR}/${dir} -s build_type=Debug
+	if [ "$?" != "0" ]; then
+		EXIT_CODE=1
+	fi
+done
+
+PLATFORM_OS=${PLATFORM_OS}" POSIX"
+PLATFORM_OS=${PLATFORM_OS}" STDCPP"
+for pr in ${PROFILES}; do
+	for platform in ${PLATFORM_OS}; do
+		conan build platform/test --build=missing -pr ${pr} -of ${COVERAGE_BUILD_DIR}/${dir} -s build_type=Debug -o platform_os=${platform}
 		if [ "$?" != "0" ]; then
 			EXIT_CODE=1
 		fi
