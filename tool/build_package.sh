809a1a24c2746822ce40696f10be18948a46ffce
katono <katono123@gmail.com>
2024-01-09 23:27:00 +0900

    Fix build_package.sh

--- tool/build_package.sh
+++ tool/build_package.sh
@@ -14,21 +14,18 @@ PACKAGE_DIRS=${PACKAGE_DIRS}" mechanism"
 PACKAGE_DIRS=${PACKAGE_DIRS}" platform"
 PROFILES=${PROFILES}" tool/profiles/profile_cppstd11"
 PROFILES=${PROFILES}" tool/profiles/profile_cppstd11_noexceptions"
-BUILD_TYPE="Debug Release"
 for dir in ${PACKAGE_DIRS}; do
 	for pr in ${PROFILES}; do
-		for type in ${BUILD_TYPE}; do
-			conan build ${dir} --build=missing -pr ${pr} -s build_type=${type}
-			if [ "$?" != "0" ]; then
-				EXIT_CODE=1
-				continue
-			fi
-			conan create ${dir} --build=missing -pr ${pr} -s build_type=${type}
-			if [ "$?" != "0" ]; then
-				EXIT_CODE=1
-				continue
-			fi
-		done
+		conan build ${dir} --build=missing -pr ${pr}
+		if [ "$?" != "0" ]; then
+			EXIT_CODE=1
+			continue
+		fi
+		conan create ${dir} --build=missing -pr ${pr}
+		if [ "$?" != "0" ]; then
+			EXIT_CODE=1
+			continue
+		fi
 	done
 done
 
